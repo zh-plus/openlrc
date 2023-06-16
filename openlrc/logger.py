016ac45ef6cfef32658877b7aa34c285e1a03c06
zh-plus <zhenghaosustc@gmail.com>
2023-06-16 10:13:53 +0800

    Update logger with colored format.

--- openlrc/logger.py
+++ openlrc/logger.py
@@ -1,9 +1,26 @@
 import logging
 
-from rich.logging import RichHandler
+from colorlog import ColoredFormatter
 
-logging.basicConfig(
-    level="INFO", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
+handler = logging.StreamHandler()
+
+formatter = ColoredFormatter(
+    "%(log_color)s [%(asctime)s] %(levelname)-8s [%(threadName)s] %(message)s",
+    datefmt='%Y-%m-%d %H:%M:%S',
+    reset=True,
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    },
+    secondary_log_colors={},
+    style='%'
 )
 
+handler.setFormatter(formatter)
 logger = logging.getLogger()
+logger.addHandler(handler)
+
+logger.setLevel('INFO')
